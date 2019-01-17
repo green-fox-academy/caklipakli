31f84f9e4731d7882517152c90d1b3617503ba98
caklipakli <caklipakli@gmail.com>
2019-01-17 13:02:06 +0100

    Exercies 1..3

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,11 @@
+public class HelloOthers {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Norbi");
+        System.out.println("Hello Bence");
+        System.out.println("Hello Viki");
+        System.out.println("Hello Krisztián");
+    }
+
+}
