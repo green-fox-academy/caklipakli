7866a139432815ee6c33a5e386019e56b8c4d781
caklipakli <caklipakli@gmail.com>
2019-01-18 11:26:09 +0100

    Exercise 24

--- week-01/day-5/src/IWontCheat.java
+++ week-01/day-5/src/IWontCheat.java
@@ -3,7 +3,7 @@ public class IWontCheat {
     public static void main(String[] args) {
 
         for (int i = 0; i < 100; i++) {
-            System.out.println("I won't cheat on the exam."); 
+            System.out.println("I won't cheat on the exam.");
         }
     }
 }
---
+++ week-01/day-5/src/PrintEven.java
@@ -0,0 +1,9 @@
+public class PrintEven {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 500; i=i+2) {
+            System.out.println(i);
+        }
+    }
+}
