4708d203bffdd0f1a4a8cd07833a739afedf7d64
caklipakli <caklipakli@gmail.com>
2019-01-21 15:18:44 +0100

    Print all exercise

---
+++ week-02/day-1/src/PrintAll.java
@@ -0,0 +1,15 @@
+public class PrintAll {
+// - Create an array variable named `numbers`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `numbers`
+
+    public static void main(String[] args) {
+
+        int[] t = {4, 5, 6, 7};
+        for (int numbers : t) {
+                 System.out.println(numbers);
+        }
+    
+    }
+    
+}
