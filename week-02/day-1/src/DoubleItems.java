defee16eca221aff2b3d62f6fd8a2c937a44e718
caklipakli <caklipakli@gmail.com>
2019-01-21 16:01:41 +0100

    Double items exercise

---
+++ week-02/day-1/src/DoubleItems.java
@@ -0,0 +1,15 @@
+public class DoubleItems {
+
+// - Create an array variable named `numList`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
+
+    public static void main(String[] args) {
+
+        int[] numList = {3, 4, 5, 6, 7};
+
+        for (int newNum : numList) {
+            System.out.println(newNum * 2);
+        }
+    }
+}
