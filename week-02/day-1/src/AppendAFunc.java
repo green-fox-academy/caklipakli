7368340649caa1033771e05338f0be6a429a6086
caklipakli <caklipakli@gmail.com>
2019-01-21 13:44:09 +0100

    AppendA

---
+++ week-02/day-1/src/AppendAFunc.java
@@ -0,0 +1,20 @@
+public class AppendAFunc {
+
+
+// - Create a string variable named `typo` and assign the value `Chinchill` to it
+// - Write a function called `appendAFunc` that gets a string as an input,
+//   appends an 'a' character to its end and returns with a string
+//
+// - Print the result of `appendAFunc(typo)`
+    public static void main(String[] args) {
+
+        String typo = "Chinchill";
+        System.out.println(appendAFunc(typo));
+    }
+    public static String appendAFunc(String text) {
+
+        String newText = text + "a";
+        return newText;
+    }
+}
+
