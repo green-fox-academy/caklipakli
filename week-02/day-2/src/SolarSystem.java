369a4016cd03040503ae797b87a01d2a28b1e519
caklipakli <caklipakli@gmail.com>
2019-01-22 14:44:19 +0100

    Solar system

---
+++ week-02/day-2/src/SolarSystem.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+    public class SolarSystem{
+        public static void main(String... args){
+            ArrayList<String> planetList = new ArrayList<>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+            // Create a method called putSaturn() which has list parameter and returns the correct list.
+
+            System.out.println(putSaturn(planetList));
+            // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+        }
+
+        public static ArrayList<String> putSaturn(ArrayList<String> planets) {
+
+            int i = planets.indexOf("Uranus");
+            planets.add(i, "Saturn");
+            return planets;
+
+        }
+
+}
