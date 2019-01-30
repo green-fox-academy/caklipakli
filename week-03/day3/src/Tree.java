92cd616563254960ad320f3391b8fbae586fdd4b
caklipakli <caklipakli@gmail.com>
2019-01-30 11:03:07 +0100

    HW 5 tree

---
+++ week-03/day3/src/Tree.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Tree {
+
+    public static void main(String[] args) {
+
+
+        List<HashMap> trees = new List<HashMap<String, String[5]>>();
+
+        String[] typeOfTree = new String[5];
+        typeOfTree[0] = "Bükk";
+        typeOfTree[1] = "Ginko";
+        typeOfTree[2] = "Tölgy";
+        typeOfTree[3] = "Szil";
+        typeOfTree[4] = "Naspolya";
+        String[] colorLeaf = new String[5];
+        colorLeaf[0] = "brown";
+        colorLeaf[1] = "yellow";
+        colorLeaf[2] = "red";
+        colorLeaf[3] = "purple";
+        colorLeaf[4] = "green";
+        int[] age = new int[5];
+        age[0] = 30;
+        age[1] = 40;
+        age[2] = 40;
+        age[3] = 40;
+        age[4] = 40;
+        boolean[] sex = new boolean[5];
+        sex[0] = false;
+        sex[1] = false;
+        sex[2] = true;
+        sex[3] = true;
+        sex[4] = false;
+
+
+
+
+    }
+
+
+}
