a4f6229de3cdd7697640ebf1b10ee2e09eb11f28
caklipakli <caklipakli@gmail.com>
2019-01-30 11:32:03 +0100

    Package creation, PostIt class

---
+++ week-03/day3/src/classesExample/Classes.java
@@ -0,0 +1,22 @@
+package classesExample;
+
+public class Classes {
+    public static void main(String[] args) {
+
+        PostIt postIt1 = new PostIt();
+        postIt1.backgroundColor = "BLUE";
+        postIt1.text = "Idea 1";
+        postIt1.textColor = "Orange";
+
+        PostIt postIt2 = new PostIt();
+        postIt2.backgroundColor = "BLACK";
+        postIt2.text = "Awesome";
+        postIt2.textColor = "Pink";
+
+        PostIt postIt3 = new PostIt();
+        postIt3.backgroundColor = "YELLOW";
+        postIt3.text = "Superb!";
+        postIt3.textColor = "Green";
+
+    }
+}
---
+++ week-03/day3/src/classesExample/PostIt.java
@@ -0,0 +1,11 @@
+package classesExample;
+
+public class PostIt {
+    // backgroundColor
+    //text on it
+    //textColor
+    String backgroundColor;
+    String text;
+    String textColor;
+
+}
