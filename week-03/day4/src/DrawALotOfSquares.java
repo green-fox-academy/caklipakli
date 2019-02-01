2b785e9faba90901d72f8e84b16baf1366f481bb
caklipakli <caklipakli@gmail.com>
2019-02-01 17:45:08 +0100

    Squares to be continued

--- week-03/day4/src/DrawALotOfSquares.java
+++ week-03/day4/src/DrawALotOfSquares.java
@@ -13,38 +13,31 @@ public class DrawALotOfSquares {
         g.fillRect(0, 0, WIDTH, HEIGHT);
         g.setColor(Color.BLACK);
 
-        drawSquares(g, 0, 0, 1);
+        drawPattern(g, 0, 0, 3, WIDTH);
 
     }
 
-    public static void drawSquares(Graphics g, int x, int y, int divide){
+    public static void drawLines(Graphics g, int x, int y, int divide, int width){
 
+            int space = width / divide;
 
-            int side = WIDTH / divide;
-
-            if (side > 30) {
-
-                for (int i = 0; i < divide ; i++) {
-
-                    for (int j = 0; j < divide; j++) {
-
-                        g.drawRect(x + j * side, y + i * side, side, side);
-                    }
-
-
-                }
-                drawSquares(g, x , y , divide * 3);}
-
-
-
-            else {
-                return;
-            }
-
+            g.drawLine(x + space, y, x + space, width);
+            g.drawLine(x + 2 * space, y, x +  2 * space, width);
+            g.drawLine(x , y + space, width, y + space);
+            g.drawLine(x , y + 2 * space, width, y + 2 * space);
 
     }
+    public static void drawPattern(Graphics g, int x, int y, int divide, int width) {
 
+        drawLines(g, x, y, divide, width);
 
+        width = width / 3;
+
+        drawLines(g, x + width, y + width, divide, width);
+
+        //drawLines(g, x , y + width, divide, width);
+
+    }
 
     // Don't touch the code below
     static int WIDTH = 320;
