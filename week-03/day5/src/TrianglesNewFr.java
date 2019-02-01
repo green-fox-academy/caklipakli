d424d516d3c12b1097307a326aec46e058696002
caklipakli <caklipakli@gmail.com>
2019-02-01 12:59:41 +0100

    Small changes in triangles, not ready

--- week-03/day5/src/TrianglesNewFr.java
+++ week-03/day5/src/TrianglesNewFr.java
@@ -10,43 +10,47 @@ public class TrianglesNewFr {
     public static void mainDraw(Graphics g) {
 
         g.drawLine(5, 5, WIDTH-5, 5);
-        g.drawLine(WIDTH-5, 5, WIDTH/2-10, HEIGHT-5);
-        g.drawLine(WIDTH/2-10, HEIGHT-5, 5, 5);
+        g.drawLine(WIDTH-5, 5, WIDTH / 2, HEIGHT-5);
+        g.drawLine(WIDTH / 2, HEIGHT-5, 5, 5);
 
 
 
         int a = (WIDTH-10) / 2;
         int h = (HEIGHT - 10) / 2;
         int startY = 5;
-        int startX = 5 + WIDTH / 2;
-
-        drawLines(g, startX, startY, a, h);
-
-        for (int i = 0; i < 5; i++) {
+        int startX = WIDTH / 2;
 
 
-            drawLines(g, startX - a/2, startY, a /2, h / 2);
-            drawLines(g, startX + a/2, startY, a / 2, h / 2);
-            drawLines(g, startX, startY + h , a / 2, h /2);
 
-            a = a / 2;
-            h = h / 2;
+            drawTriangle(g, startX, startY, a, h);
+            drawTriangles(g, startX, startY, a, h);
+
+
+        
+    }
+
+    public static void drawTriangle(Graphics g, int startX, int startY, int a, int h){
+
+        g.drawLine(startX, startY, startX - a/2, startY + h);
+        g.drawLine(startX - a/2, startY + h, startX + a/2, startY + h);
+        g.drawLine(startX + a/2, startY + h, startX, startY);
+
+    }
+
+    public static void drawTriangles (Graphics g, int startX, int startY, int a, int h) {
+
+        if (a > 1) {
+        drawTriangle(g, startX - a / 2, startY, a / 2, h / 2);
+        drawTriangle(g, startX + a / 2, startY, a / 2, h / 2);
+        drawTriangle(g, startX, startY + h, a / 2, h / 2);
+
 
         }
+
+        else {
+            return;
+        }
     }
-
-    public static void drawLines(Graphics g, int startX, int startY, int a, int h){
-
-
-
-                g.drawLine(startX, startY, startX - a/2, startY + h);
-                g.drawLine(startX - a/2, startY + h, startX + a/2, startY + h);
-                g.drawLine(startX + a/2, startY + h, startX, startY);
-
-
-
-    }
-
 
     // Don't touch the code below
     static int WIDTH = 380;
