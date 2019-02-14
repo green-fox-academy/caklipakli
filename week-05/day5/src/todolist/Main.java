b53beb89f15cd88e72ac945a9521425b8439ee1d
caklipakli <caklipakli@gmail.com>
2019-02-14 11:27:26 +0100

    Carrier exercise started

---
+++ week-05/day5/src/todolist/Main.java
@@ -0,0 +1,14 @@
+package todolist;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        if (args[0] == "-") {
+
+            System.out.println("Command Line Todo application");
+
+        }
+    }
+}
