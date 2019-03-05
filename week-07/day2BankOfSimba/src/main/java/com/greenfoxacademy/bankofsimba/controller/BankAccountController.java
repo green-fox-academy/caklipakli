ebdd2e7ff611dd2e9eecad5edd5b2debf191caa0
caklipakli <caklipakli@gmail.com>
2019-03-05 15:24:47 +0100

    Bank of Simba continue

--- week-07/day2BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
+++ week-07/day2BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
@@ -4,16 +4,48 @@ import com.greenfoxacademy.bankofsimba.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BankAccountController {
 
     BankAccount accountSimba = new BankAccount("Simba", 2000.5687f, "lion", "zebra");
+    BankAccount accountZazu = new BankAccount("Zazu", 200.5687f, "hornbill", "fruit");
+    BankAccount accountNala = new BankAccount("Nala", 2005.5687f, "lion", "zebra");
+    BankAccount accountTimon = new BankAccount("Timon", 20005.5687f, "meerkats", "insect");
+    BankAccount accountMuszafa = new BankAccount("Muszafa", 1000.5687f, "lion", "zebra");
+
+    List<BankAccount> bankAccounts = new ArrayList<>();
+
+
 
     @RequestMapping("/show")
     public String show(Model model){
 
         model.addAttribute("account", accountSimba);
         return "index";
+    }
+
+    @RequestMapping("/htmltext")
+    public String htmlText(Model model){
+
+        model.addAttribute("message", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+        return "htmltext";
+    }
+
+    @RequestMapping("/accounts")
+    public String showAccounts(Model model){
+
+        bankAccounts.add(accountSimba);
+        bankAccounts.add(accountMuszafa);
+        bankAccounts.add(accountNala);
+        bankAccounts.add(accountTimon);
+        bankAccounts.add(accountZazu);
+
+        model.addAttribute("accountsList", bankAccounts);
+        return "accounts";
     }
 }
