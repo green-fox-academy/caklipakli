207e5e017d7170efbb255ccb5e057ea1756d9488
caklipakli <caklipakli@gmail.com>
2019-03-14 10:49:58 +0100

    Reddit project init

---
+++ week-08/day4/src/main/java/com/greenfoxacademy/reddit/RedditApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditApplication.class, args);
+    }
+
+}
