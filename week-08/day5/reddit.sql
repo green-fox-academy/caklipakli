e16b56d02ba1da26db3926e9917fafed374ae82f
caklipakli <caklipakli@gmail.com>
2019-03-18 13:11:22 +0100

    Orientation example exam initialized

---
+++ week-08/day5/reddit.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE reddit;
+
+USE reddit;
+drop table reddit_post;
---
+++ week-08/day5/src/main/java/com/greenfoxacademy/orientationexample/OrientationexampleApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.orientationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientationexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientationexampleApplication.class, args);
+    }
+
+}
